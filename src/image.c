d4c33e1026a0c025531c19d2b5cc99fdb8058bae
Wadah Elsir <wael00001@stud.uni-saarland.de>
2024-05-19 12:03:16 +0200

    done with the apply threshold task

--- src/image.c
+++ src/image.c
@@ -7,16 +7,32 @@
 #include <stdlib.h>
 #include <string.h>
 
-void apply_threshold(float *img, int w, int h, int T) {
+void apply_threshold(float *img, int w, int h, int T)
+{
     (void)img;
     (void)w;
     (void)h;
     (void)T;
 
     // TODO: Implement me!
+    for (int i = 0; i < h; i++)
+    {
+        for (int j = 0; j < w; j++)
+        {
+            if (*(img + (i * w + j)) > T)
+            {
+                *(img + (i * w + j)) = 255;
+            }
+            else
+            {
+                *(img + (i * w + j)) = 0;
+            }
+        }
+    }
 }
 
-void scale_image(float *result, const float *img, int w, int h) {
+void scale_image(float *result, const float *img, int w, int h)
+{
     (void)result;
     (void)img;
     (void)w;
@@ -25,7 +41,8 @@ void scale_image(float *result, const float *img, int w, int h) {
     // TODO: Implement me!
 }
 
-float get_pixel_value(const float *img, int w, int h, int x, int y) {
+float get_pixel_value(const float *img, int w, int h, int x, int y)
+{
     (void)img;
     (void)w;
     (void)h;
@@ -37,7 +54,8 @@ float get_pixel_value(const float *img, int w, int h, int x, int y) {
     return 0;
 }
 
-float *array_init(int size) {
+float *array_init(int size)
+{
     (void)size;
 
     // TODO: Implement me!
@@ -45,13 +63,15 @@ float *array_init(int size) {
     return NULL;
 }
 
-void array_destroy(float *m) {
+void array_destroy(float *m)
+{
     (void)m;
 
     // TODO: Implement me!
 }
 
-float *read_image_from_file(const char *filename, int *w, int *h) {
+float *read_image_from_file(const char *filename, int *w, int *h)
+{
     (void)filename;
     (void)w;
     (void)h;
@@ -61,7 +81,8 @@ float *read_image_from_file(const char *filename, int *w, int *h) {
     return NULL;
 }
 
-void write_image_to_file(const float *img, int w, int h, const char *filename) {
+void write_image_to_file(const float *img, int w, int h, const char *filename)
+{
     (void)img;
     (void)w;
     (void)h;
